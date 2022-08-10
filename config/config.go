990ac93fc9f4404130d3d22e57ecd7c9fd1a3fa3
Eoin Motherway <25342760+YuKitsune@users.noreply.github.com>
2022-08-10 12:28:06 +1000

    Tidy up config

--- config/config.go
+++ config/config.go
@@ -1,10 +1,13 @@
 package config
 
 import (
+	"fmt"
 	"github.com/fsnotify/fsnotify"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
+	"io"
 	"strings"
+	"time"
 )
 
 func Setup(logger logrus.FieldLogger) (Config, error) {
@@ -47,4 +50,56 @@ func Setup(logger logrus.FieldLogger) (Config, error) {
 	}
 
 	return NewConfigProvider(v), nil
+}
+
+type Config interface {
+	ScrapeInterval() time.Duration
+	Prometheus() Prometheus
+	Database() Database
+	Bot() Bot
+	Log() Log
+	Debug(w io.Writer)
+}
+
+type viperConfig struct {
+	v    *viper.Viper
+	prom *viperPrometheusConfig
+	db   *viperDatabaseConfig
+	bot  *viperBotConfig
+	log  *viperLogConfig
+}
+
+func NewConfigProvider(v *viper.Viper) Config {
+	return &viperConfig{
+		v:    v,
+		prom: &viperPrometheusConfig{v},
+		db:   &viperDatabaseConfig{v},
+		bot:  &viperBotConfig{v},
+		log:  &viperLogConfig{v},
+	}
+}
+
+func (c *viperConfig) ScrapeInterval() time.Duration {
+	intervalMinutes := c.v.GetInt("scrapeIntervalMinutes")
+	return time.Duration(intervalMinutes) * time.Minute
+}
+
+func (c *viperConfig) Prometheus() Prometheus {
+	return c.prom
+}
+
+func (c *viperConfig) Database() Database {
+	return c.db
+}
+
+func (c *viperConfig) Bot() Bot {
+	return c.bot
+}
+
+func (c *viperConfig) Log() Log {
+	return c.log
+}
+
+func (c *viperConfig) Debug(w io.Writer) {
+	fmt.Fprintf(w, "%#v", c.v.AllSettings())
 }
